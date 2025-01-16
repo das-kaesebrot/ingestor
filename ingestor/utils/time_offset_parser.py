efbe82458692ea1f7ecafb6b8e2f3bb9f0d8e386
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2025-01-16 09:31:52 +0100

    formatting

--- ingestor/utils/time_offset_parser.py
+++ ingestor/utils/time_offset_parser.py
@@ -5,32 +5,39 @@ from datetime import timedelta, datetime
 
 class TimeOffsetParser:
     REGEX_TIMEOFFSET_FORMAT = r"^(\+?|-)(\d{2}):(\d{2}):(\d{2})$"
-    
+
     def __init__(self):
         pass
-    
+
     @staticmethod
     def parse(value: str) -> timedelta:
         value = value.strip()
         if not re.match(pattern=TimeOffsetParser.REGEX_TIMEOFFSET_FORMAT, string=value):
             raise argparse.ArgumentTypeError("Time offset is not in correct format")
-        
+
         positive = True
-        if value.startswith("-"): positive = False        
+        if value.startswith("-"):
+            positive = False
         value = value.lstrip("+").lstrip("-")
-        
+
         parsed_date: datetime
-        
+
         try:
             parsed_date = datetime.strptime(value, "%H:%M:%S")
         except Exception as e:
-            raise argparse.ArgumentTypeError("Failed parsing time offset!").with_traceback(e.__traceback__)
-        
-        delta = timedelta(hours=parsed_date.hour, minutes=parsed_date.minute, seconds=parsed_date.second)
-        
+            raise argparse.ArgumentTypeError(
+                "Failed parsing time offset!"
+            ).with_traceback(e.__traceback__)
+
+        delta = timedelta(
+            hours=parsed_date.hour,
+            minutes=parsed_date.minute,
+            seconds=parsed_date.second,
+        )
+
         if not positive:
             zero = timedelta(seconds=0)
-            
+
             delta = zero - delta
-        
+
         return delta
