c92a80fcf5ff9cd03df7c7f01f58c7413d39fbb5
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2025-05-09 16:22:13 +0200

    add htmx django package and example base template

--- ui/views.py
+++ ui/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
+from django.views.decorators.http import require_GET
+from django_htmx.middleware import HtmxDetails
 
+class HtmxHttpRequest(HttpRequest):
+    htmx: HtmxDetails
 
-def index(request):
-    return HttpResponse("Hello, world")
+@require_GET
+def index(request: HtmxHttpRequest) -> HttpResponse:
+    return render(request, "index.html")
