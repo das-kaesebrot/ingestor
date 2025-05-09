1290184e5c561dd030242b9181964b95b9d1e35c
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2025-05-09 16:00:53 +0200

    use ui paths without prefix

--- django_site/urls.py
+++ django_site/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("ui/", include("ui.urls")),
+    path("", include("ui.urls")),
     path("admin/", admin.site.urls),
 ]
