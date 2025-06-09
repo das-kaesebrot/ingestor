dec6e64251258c52b05ae37490f22186a05ef757
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2025-06-09 23:35:40 +0200

    add initial models

--- ui/models.py
+++ ui/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=128)
+    
+class Project(models.Model):
+    name = models.CharField(max_length=30)
+    created_at = models.DateField()
+    members = models.ManyToManyField(Person)
