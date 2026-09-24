a85a8405f2731fb5311501ca58c8a003b1634909
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2026-09-24 23:03:58 +0200

    feat(frontend): add initial error page

---
+++ web/app/src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { page } from '$app/state';
+
+	let { status } = $props();
+</script>
+
+<svelte:head>
+	<title>Error {status ?? page.status}</title>
+	<meta name="robots" content="noindex,noimageindex" />
+</svelte:head>
+
+<h1>{status ?? page.status} {page.error?.message}</h1>
+<span style="font-size: 10em">
+	{status ?? page.status}
+</span>
--- web/app/src/routes/+layout.svelte
+++ web/app/src/routes/+layout.svelte
@@ -5,5 +5,9 @@
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<link href="/error" />
+
+</svelte:head>
 {@render children()}
